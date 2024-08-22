363fa606df25cb9ecd42c2c5e7ba70b3e61318d8
qqice <39629131+qqice@users.noreply.github.com>
2024-08-22 17:24:56 +0800

    fix3

--- real_time_processing_rknn.py
+++ real_time_processing_rknn.py
@@ -67,7 +67,7 @@ for idx in range(num_blocks):
     # set block to input
     model_inputs_1[0] = in_mag
     # run calculation 
-    model_outputs_1 = interpreter_1.inference(inputs=model_inputs_1,data_format='NCHW')
+    model_outputs_1 = interpreter_1.inference(inputs=model_inputs_1)
     # get the output of the first block
     out_mask = model_outputs_1[0]
     # set out states back to input
@@ -81,7 +81,7 @@ for idx in range(num_blocks):
     # interpreter_2.set_tensor(input_details_1[1]['index'], states_2)
     model_inputs_2[0] = estimated_block
     # run calculation
-    model_outputs_2 = interpreter_2.inference(inputs=model_inputs_2,data_format='NCHW')
+    model_outputs_2 = interpreter_2.inference(inputs=model_inputs_2)
     # get output
     out_block = model_outputs_2[0]
     # set out states back to input
